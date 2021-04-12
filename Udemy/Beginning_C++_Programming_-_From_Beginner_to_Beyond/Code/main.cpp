15ad9b904e506b5d48b7203d06b714df7afb9651
DavidCorzo <davidcorzo@ufm.edu>
2021-04-11 19:47:07 -0600

    c++ advance

--- Udemy/Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
+++ Udemy/Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
@@ -1,69 +1,65 @@
 #include <iostream>
 using namespace std;
 
-class Account {
-private:
-    string name;
-    double balance;
-public:
-    void set_balance(double bal) {balance = bal;}
-    double get_balance() {return balance;}
-
-    void set_name(string n);
-    string get_name();
-    bool deposit(double amount);
-    bool withdraw(double amount);
+class Player {
+    private:
+    std::string name;
+    int health;
+    int xp;
+    public: 
+    std::string get_name() {return name;}
+    int get_health() {return health;}
+    int get_xp() {return xp;}
+    Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
+    // Copy constructor:
+    Player(const Player &source);
+    // Destructor:
+    ~Player() { cout << "Destrocutor called for: " << name << endl; }
 };
 
-void Account::set_name(string n) {
-    name = n;
-}
-string Account::get_name() {
-    return name;
-}
-bool Account::deposit(double amount) {
-    balance += amount;
-    return true;
-}
-bool Account::withdraw(double amount) {
-    if (balance-amount >= 0) {
-        balance -= amount;
-        return true;
-    }
-    else {
-        return false;
-    }
+Player::Player(std::string name_val, int health_val, int xp_val) : name{name_val}, health{health_val}, xp{xp_val} {
+    cout << "Theree-args constructor for " + name << endl;
 }
 
+Player::Player(const Player &source) : name{source.name}, health{source.health}, xp{source.xp} {
+    cout << "Copy constructor - made a copy of: " << source.name << endl;
+}
+
+void display_player(Player p) {
+    cout << "Name: " << p.get_name() << endl;
+    cout << "Health: " << p.get_health() << endl;
+    cout << "xp: " << p.get_xp() << endl;
+}
 
 int main() {
-    Account frank_account;
-    frank_account.set_name("Frank's account");
-    frank_account.set_balance(1000.00);
-    if (frank_account.deposit(200.00)) {
-        cout << "Deposit OK" << endl;
-    }
-    else {
-        cout << "Deposit Not allowed." << endl;
-    }
-
-    if (frank_account.withdraw(500.00)) {
-        cout << "Withdraw OK" << endl;
-    } 
-    else {
-        cout << "Not sufficient funds." << endl;
-    }
-
-    if (frank_account.withdraw(1500.00)) {
-        cout << "Withdraw OK" << endl;
-    }
-    else {
-        cout << "Not sufficient funds" << endl;
-    }
+    Player empty;
+    display_player(empty);
+    Player copy_of_empty {empty};
+    display_player(copy_of_empty);
+    Player frank {"Frank"};
+    Player hero {"Hero", 100};
+    Player villain {"Villain", 100, 55};
     return 0;
 }
 /* OUTPUT:
-Deposit OK
-Withdraw OK
-Not sufficient funds
+Theree-args constructor for None
+Copy constructor - made a copy of: None
+Name: None
+Health: 0
+xp: 0
+Destrocutor called for: None
+Copy constructor - made a copy of: None
+Copy constructor - made a copy of: None
+Name: None
+Health: 0
+xp: 0
+Destrocutor called for: None
+Theree-args constructor for Frank
+Theree-args constructor for Hero
+Theree-args constructor for Villain
+Destrocutor called for: Villain
+Destrocutor called for: Hero
+Destrocutor called for: Frank
+Destrocutor called for: None
+Destrocutor called for: None
 */
